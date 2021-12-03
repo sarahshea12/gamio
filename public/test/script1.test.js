eba8bb522c3b971bdf1f2141699cfed1d782b0ad
Tina <cmodica@uncc.edu>
2021-12-03 17:42:53 -0500

    adding tests

--- public/test/script1.test.js
+++
@@ -1,9 +0,0 @@
-const { expect } = require('@jest/globals');
-const script = require('../script.js'); //import script to be tested
-
-//test for Hello World
-
-
-test("returns Hello world", () => {
-    expect(script.helloWorld()).toBe("Hello world!")
-})
